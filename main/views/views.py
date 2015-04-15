a0dd1a38f536cbd0841c2b260c3520355c9c2e3b
Dylan Grayson <dgrayson11@gmail.com>
2015-04-14 23:29:41 -0700

    basic stock login system

--- main/views/views.py
+++ main/views/views.py
@@ -1,10 +1,16 @@
 
 import webapp2
 
+from google.appengine.api import users
+
 class Home(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Knobsock in this bish')
+		user = users.get_current_user()
+		if user:
+			self.response.headers['Content-Type'] = 'text/plain'
+			self.response.write('Knobsock in this bish ' + user.email())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 
 app = webapp2.WSGIApplication([
