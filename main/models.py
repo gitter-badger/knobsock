ebf22eb6f7b458b45c221cfb6196720fe1806927
Dylan Grayson <dgrayson11@gmail.com>
2015-04-15 09:38:59 -0700

    group and knob models created

--- main/models.py
+++ main/models.py
@@ -3,4 +3,10 @@ from google.appengine.api import users
 
 class Group(ndb.Model):
 	name = ndb.StringProperty()
-	members = ndb.StructuredProperty(users, repeated=true)
+	members = ndb.UserProperty(repeated=True)
+
+class Knob(ndb.Model):
+	name = ndb.StringProperty()
+	parent = ndb.StructuredProperty(Group)
+	sock = ndb.BooleanProperty()
+	timeout = ndb.DateTimeProperty()
--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 
-import webapp2
-
 from google.appengine.api import users
+import webapp2
+from main import models
 
 class Home(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.write('Knobsock in this bish ' + user.email())
+			self.response.headers['Content-Type'] = 'text/html'
+			self.response.write('<h1>Knobsock in this bish ' + user.email() + '</h1>')
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 
