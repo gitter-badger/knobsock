ba3bc25d25ffb28086a8633d2f256c5134edd97e
Conner Swann <me@connerswann.me>
2015-04-16 09:12:46 -0700

    added models and some housekeeping functions

--- model.py
+++ model.py
@@ -1,26 +1,54 @@
 from google.appengine.ext import ndb
 
-class Guest(ndb.Model):
-  first = ndb.StringProperty()
-  last = ndb.StringProperty()
+class Group(ndb.Model):
+  name = ndb.StringProperty()
+  members = ndb.UserProperty(repeated=True)
+
+class Knob(ndb.Model):
+  name = ndb.StringProperty()
+  parent = ndb.KeyProperty()
+  timeout = ndb.DateTimeProperty()
+  sock = ndb.BooleanProperty()
 
 
-def AllGuests():
-  return Guest.query()
+### Knob Functions ###
+# get all child knobs of a group
+def ListKnobs(parent):
+  return Knob.query(parent=parent)
 
+# update values of a knob with given ID
+def UpdateKnob(id, name, timeout, sock):
+  knob = Knob(id=id, name=name, timeout=timeout, sock=sock)
+  knob.put()
+  return knob
 
-def UpdateGuest(id, first, last):
-  guest = Guest(id=id, first=first, last=last)
-  guest.put()
-  return guest
+# create brand new knob
+# defaults to no sock
+def CreateKnob(name):
+  knob = Knob(name=name, sock=False)
+  knob.put()
+  return knob
 
-
-def InsertGuest(first, last):
-  guest = Guest(first=first, last=last)
-  guest.put()
-  return guest
-
-
-def DeleteGuest(id):
-  key = ndb.Key(Guest, id)
+# deletes a knob
+def DeleteKnob(id):
+  key = ndb.Key(Knob, id)
   key.delete()
+
+### Group Functions ###
+# update values of a group with given ID
+def UpdateGroup(id, name, members):
+  group = Group(id=id, name=name, members=members)
+  group.put()
+  return group
+
+# create brand new group
+# members list consists of the logged-in user
+def CreateGroup(name):
+  user = users.get_current_user()
+  if user:
+    group = Group(name=name, members=[user])
+    group.put()
+  else: 
+    # there needs to be a logged-in user to own this group
+    pass 
+  return group
