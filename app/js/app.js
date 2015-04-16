cfcefb4cea50f1fef6338f8b5cbd91b6461775b9
Conner Swann <me@connerswann.me>
2015-04-16 11:41:50 -0700

    Added splash page and some basic controllers to Angular app

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,8 @@
 'use strict';
 
-var App = angular.module('App', ['ngRoute']);
+var KnobSock = angular.module('KnobSock', ['ngRoute']);
 
-App.factory('myHttpInterceptor', function($rootScope, $q) {
-  return {
-    'requestError': function(config) {
-      $rootScope.status = 'HTTP REQUEST ERROR ' + config;
-      return config || $q.when(config);
-    },
-    'responseError': function(rejection) {
-      $rootScope.status = 'HTTP RESPONSE ERROR ' + rejection.status + '\n' +
-                          rejection.data;
-      return $q.reject(rejection);
-    },
-  };
-});
-
-App.factory('guestService', function($rootScope, $http, $q, $log) {
+KnobSock.factory('guestService', function($rootScope, $http, $q, $log) {
   $rootScope.status = 'Retrieving data...';
   var deferred = $q.defer();
   $http.get('rest/query')
@@ -28,62 +14,30 @@ App.factory('guestService', function($rootScope, $http, $q, $log) {
   return deferred.promise;
 });
 
-App.config(function($routeProvider) {
+KnobSock.config(function($routeProvider) {
   $routeProvider.when('/', {
-    controller : 'MainCtrl',
-    templateUrl: '/partials/main.html',
-    resolve    : { 'guestService': 'guestService' },
+    controller: 'SplashCtrl',
+    templateUrl: '/partials/splash.html',
   });
-  $routeProvider.when('/invite', {
-    controller : 'InsertCtrl',
-    templateUrl: '/partials/insert.html',
-  });
-  $routeProvider.when('/update/:id', {
-    controller : 'UpdateCtrl',
-    templateUrl: '/partials/update.html',
-    resolve    : { 'guestService': 'guestService' },
+  $routeProvider.when('/dashboard', {
+    controller: 'DashCtrl',
+    templateUrl: '/partials/dashboard.html',
   });
   $routeProvider.otherwise({
-    redirectTo : '/'
+    redirectTo: '/'
   });
 });
 
-App.config(function($httpProvider) {
-  $httpProvider.interceptors.push('myHttpInterceptor');
-});
-
-App.controller('MainCtrl', function($scope, $rootScope, $log, $http, $routeParams, $location, $route) {
-
-  $scope.invite = function() {
-    $location.path('/invite');
-  };
-
-  $scope.update = function(guest) {
-    $location.path('/update/' + guest.id);
-  };
-
-  $scope.delete = function(guest) {
-    $rootScope.status = 'Deleting guest ' + guest.id + '...';
-    $http.post('/rest/delete', {'id': guest.id})
-    .success(function(data, status, headers, config) {
-      for (var i=0; i<$rootScope.guests.length; i++) {
-        if ($rootScope.guests[i].id == guest.id) {
-          $rootScope.guests.splice(i, 1);
-          break;
-        }
-      }
-      $rootScope.status = '';
-    });
-  };
+KnobSock.controller('SplashCtrl', function($scope, $rootScope, $log, $http, $routeParams, $location, $route) {
 
 });
 
-App.controller('InsertCtrl', function($scope, $rootScope, $log, $http, $routeParams, $location, $route) {
+KnobSock.controller('DashCtrl', function($scope, $rootScope, $log, $http, $routeParams, $location, $route) {
 
   $scope.submitInsert = function() {
     var guest = {
-      first : $scope.first,
-      last : $scope.last, 
+      first: $scope.first,
+      last: $scope.last, 
     };
     $rootScope.status = 'Creating...';
     $http.post('/rest/insert', guest)
@@ -93,31 +47,5 @@ App.controller('InsertCtrl', function($scope, $rootScope, $log, $http, $routePar
     });
     $location.path('/');
   }
-});
-
-App.controller('UpdateCtrl', function($routeParams, $rootScope, $scope, $log, $http, $location) {
-
-  for (var i=0; i<$rootScope.guests.length; i++) {
-    if ($rootScope.guests[i].id == $routeParams.id) {
-      $scope.guest = angular.copy($rootScope.guests[i]);
-    }
-  }
-
-  $scope.submitUpdate = function() {
-    $rootScope.status = 'Updating...';
-    $http.post('/rest/update', $scope.guest)
-    .success(function(data, status, headers, config) {
-      for (var i=0; i<$rootScope.guests.length; i++) {
-        if ($rootScope.guests[i].id == $scope.guest.id) {
-          $rootScope.guests.splice(i,1);
-          break;
-        }
-      }
-      $rootScope.guests.push(data);
-      $rootScope.status = '';
-    });
-    $location.path('/');
-  };
-
 });
 
